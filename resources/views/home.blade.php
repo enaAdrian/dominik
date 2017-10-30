7bac58a2e5f0375e0760de0161aaa98d09073116
Adrian <ancio5@interia.pl>
2017-10-30 15:32:12 +0100

    init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dominik</title>
+    <link rel="stylesheet" href="/css/owl.carousel.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/ionicons.min.css">
+    <link rel="stylesheet" href="/css/animate.css">
+    <link rel="stylesheet" href="/css/responsive.css">
+    <script src="/js/vendor/modernizr-2.6.2.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/owl.carousel.min.js"></script>
+    <script src="/js/plugins.js"></script>
+    <script src="/js/min/waypoints.min.js"></script>
+    <script src="/js/jquery.counterup.js"></script>
+    <script src="/js/google-map-init.js"></script>
+    <script src="/js/main.js"></script>
+
+</head>
+<body>
+    
+    <!-- Header -->
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                  <img src="img/logo2.png" alt="Logo">
+                </a>
+              </div>
+                
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html">Autor</a></li>
+                    <li><a href="work.html">Aktualności</a></li>
+                    <li><a href="#">Dyskografia</a></li>
+                    <li><a href="#">Galeria</a></li>
+                    <li><a href="contact.html">Kontakt</a></li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+        
+    <!-- Slider -->
+    <section id="slider">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-md-offset-2">
+            <div class="block">
+              <h1 class="animated fadeInUp">Dominik Kozicki <br> Muzyk</h1>
+              <p class="animated fadeInUp">Oficjalna strona </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Wrapper -->
+    <section id="intro">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-sm-12">
+            <div class="block">
+              <div class="section-title">
+                <h2>O mnie</h2>
+                <p><span style="color: #c0c0c0;">W 2006 r. pojawił się na scenie muzycznej Dominik Kozicki Dave jako jeden z wykonawc&oacute;w <a href="http://www.youtube.com/watch?v=2k-NcAfCGoc" target="_blank">studenckiej grupy perkusyjnej</a> Akademii Muzycznej w Poznaniu, na co dzień posiadającej w swoim repertuarze utwory <a href="http://www.youtube.com/watch?v=nmzyl8Fu4c8" target="_blank">klasyki perkusyjnej</a>. Będąc studentem III roku klasy perkusji, w 2008 r. zdobył I nagrodę ABSOLUTE w kategorii werbla i III nagrodę w kategorii DRUM SET (Jury w tej kategorii I nagrody nie przyznało) na VI Międzynarodowym Konkursie Perkusyjnym w Fermo we Włoszech. Zainteresowania muzyczne Dominika wykraczają poza uczelniane mury. W domowym studio tworzy projekty muzyki perkusyjnej, kt&oacute;re cechuje szeroka kolorystyka brzmieniowa, rytmika i zr&oacute;żnicowanie stylistyczne. Jest muzykiem poszukującym. Fascynują go r&oacute;żne style oraz zabawa z dźwiękiem. Inspiracje muzyczne czerpie z otaczającej natury i brzmienia instrument&oacute;w. To właśnie walory brzmieniowe instrumentu decydują o wyborze stylistyki w trakcie tworzenia nowego utworu. Muzyk jest wierny klawiszom. Pianino elektroniczne, syntezatory to kolejne pasje, kt&oacute;re rozwija &bdquo;po godzinach&rdquo; &ndash; najczęściej w nocy gdy zasady wsp&oacute;łżycia społecznego ograniczają możliwość grania na dynamicznych bębnach. Miłość do muzyki klawiszowej zrodziła się w dzieciństwie gdy Dominik odkrył taśmy z nagraniami nadanymi na przełomie lat 80 w audycjach radiowych Programu III PR. Od 2002 r. tworzy także muzykę ilustracyjną. Część jego utwor&oacute;w znalazła się na płytach &bdquo;<a href="multimedia.html">Okna czasu</a>&rdquo;, ponadto na poszerzonej wersji tego albumu "The Windows of Time" oraz (maxi-singlu) "The Dawn" - wydanych (w oparciu o własny budżet) przez Wydawnictwo Fonograficzne <a href="kontakt--wydawca.html">KK&amp;D Records</a>. Muzyka zawarta na tych krążkach odsłania wnętrze kreatywnego autora, aranżera i instrumentalisty.</span></p>
+
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5 col-sm-12">
+            <div class="block">
+              <img src="img/dominik_5.jpg" alt="Img">
+            <!-- </div> -->
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <section id="feature">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-6">
+          <p><span style="color: #c0c0c0;">W 2011 r. wprowadza w życie plan nagrania kolejnego albumu z rockowymi utworami przełomu lat 60 i 70 ubiegłego wieku. Projekt powstał z myślą o ocaleniu od zapomnienia wspaniałej muzyki cenionych tw&oacute;rc&oacute;w i wykonawc&oacute;w, m.in.: Led Zeppelin, Deep Purple, Brainbox, Breakout, Czerwone Gitary. Dominik nagrał płytę wsp&oacute;łpracując z wokalistami i instrumentalistami r&oacute;żnych pokoleń (podkarpackiej i krajowej sceny muzycznej). Premierę albumu poprzedziło wydanie singla z (budzącą emocje) wersją utworu pt. "<a href="http://www.youtube.com/watch?v=h-J-4mDqoQM">My z XX wieku</a>" <a href="http://m.trojmiasto.pl/rozrywka/Klenczon-poemat-rockowy-imp303798.html">Krzysztofa Klenczona</a>. W nagraniu płyty wziął udział <a href="http://marekpiekarczyk.pl/">Marek Piekarczyk</a> (TSA) - wokalista, kt&oacute;ry na stałe zapisał się w historii polskiego Rock and Rolla.<br /></span></p>
+          <a href="work.html" class="btn btn-view-works">Albumy</a>
+        </div>
+      </div>
+    </div>
+  </section>
+        
+    <!-- Service Start -->
+    <section id="service">
+      <div class="container">
+        <div class="row">
+          <div class="section-title">
+            <h2>Aktualności</h2>
+            
+          </div>
+        </div>
+        <div class="row ">
+          <div class="col-sm-3">
+            <div class="service-item">
+              <div class="circular"><img src="../img/logo2.png" alt="img"/>
+              </div>
+              
+            </div>
+          </div>
+          <div class="col-sm-9">
+            <div class="service-item">
+
+              <p>Lorem ipsum dolor si amt, conetur aipicing eit, do eiusmod temr incidunt ut or si amt, conetur aipicin amet, conetur aipicing ei  amet, conetur aipicing ei </p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row ">
+          <div class="col-sm-3">
+            <div class="service-item">
+              <div class="circular"><img src="../img/logo2.png" alt="img"/>
+              </div>
+              
+            </div>
+          </div>
+          <div class="col-sm-9">
+            <div class="service-item">
+
+              <p>Lorem ipsum dolor si amt, conetur aipicing eit, do eiusmod temr incidunt ut or si amt, conetur aipicin amet, conetur aipicing ei  amet, conetur aipicing ei </p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row ">
+          <div class="col-sm-3">
+            <div class="service-item">
+              <div class="circular"><img src="../img/logo2.png" alt="img"/>
+              </div>
+              
+            </div>
+          </div>
+          <div class="col-sm-9">
+            <div class="service-item">
+
+              <p>Lorem ipsum dolor si amt, conetur aipicing eit, do eiusmod temr incidunt ut or si amt, conetur aipicin amet, conetur aipicing ei  amet, conetur aipicing ei </p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Call to action Start -->
+    <section id="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="block">
+              <h2>Dyskografia Dominika Kozickiego</h2>
+              <p>Lorem ipsum dolor si amt, conetur aipicing eit, do eiusmod temr incidunt ut or si amt, conetur aipicin amet, conetur aipicing ei  amet, conetur aipicing ei</p>
+              <a class="btn btn-default btn-call-to-action" href="#" >Multimedia</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Content -->
+    <section id="testimonial">
+      <div class="container">
+        <div class="row">
+          <div class="section-title text-center">
+            <h2>Kontakt</h2>
+            <p>Lorem ipsum dolor si amt, conetur aipicing eit, do eiusmod temr incidunt ut or si amt, conetur aipicin amet, conetur aipicing ei  amet, conetur aipicing ei</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="footer-menu">
+              <ul>
+                <li><a href="#">O mnie</a></li>
+                <li><a href="#">Kontakt</a></li>
+                <li><a href="#">Dyskografia</a></li>
+              </ul>
+            </div>
+            <p>Copyright &copy; <a href="">ENA</a>.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+            
+            
+  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+</body>
+</html>
